96e63acb6efc8e6ae63a0a9a1e9100253bf40133
Andy Wickert <andy@northernwidget.com>
2021-09-13 16:06:28 -0500

    Export gpkg for all streams, buffers

---
+++ export_streams.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+# Export all streams from the final simulations to a set of gpkg files
+
+declare -a DrainageAreaArray=(4988
+3776
+13928
+4242
+3178
+10034
+3690
+2743
+7842
+6053
+4653
+27766
+7696
+6060
+62235)
+
+# Streams
+for A in ${DrainageAreaArray[@]}
+do
+    echo $A
+    v.out.ogr input=streams_$A output=streams_$A.gpkg
+done
+
+# Streams in basin
+for A in ${DrainageAreaArray[@]}
+do
+    echo $A
+    # tmp because of attributes linked to points, not lines
+    v.extract input=streams_${A} output=tmp type=line --o
+    v.overlay ainput=tmp atype=line binput=basin output=streams_${A}_inbasin operator=and --o
+    v.out.ogr input=streams_${A}_inbasin output=streams_${A}_inbasin.gpkg --o
+done
+
+# Buffers
+for A in ${DrainageAreaArray[@]}
+do
+    echo $A
+    v.out.ogr input=streams_${A}_buffered_inbasin output=streams_${A}_buffered_inbasin.gpkg --o
+done
+
+
