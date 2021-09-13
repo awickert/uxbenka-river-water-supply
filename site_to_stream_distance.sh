7561c6f3bda22cbced66a5f0e79161444e5bd467
Andy Wickert <andy@northernwidget.com>
2021-09-13 11:37:53 -0500

    distance to rivers text + add more thresholds

--- site_to_stream_distance.sh
+++ site_to_stream_distance.sh
@@ -16,18 +16,45 @@ declare -a DrainageAreaArray=(4988
 6060
 62235)
 
-
 # Import the Plazuela locations
 v.in.ogr input=plazuelas.gpkg output=plazuelas --o
 
 for A in ${DrainageAreaArray[@]}
 do
     echo $A
-    v.db.addcolumn map=plazuelas columns="$A double precision"
-    v.distance from=plazuelas to=streams_$A upload=dist column=$A
+    v.db.addcolumn map=plazuelas columns="distance_to_rivers_$A double precision"
+    v.distance from=plazuelas to=streams_$A upload=dist column=distance_to_rivers_$A
 done
 
 v.db.select map=plazuelas sep='\t' file=plazuelas.tsv
 
 # And then I did the rest of the means, edits, etc. by hand
+
+
+# Let's flesh out the rest of this with the other numbers
+declare -a DrainageAreaArray=(923
+699
+2577
+785
+588
+1856
+683
+507
+1451
+1120
+861
+5137
+1424
+1121
+11513
+500 1000 2000 4000 8000 16000 32000 64000 128000)
+
+for A in ${DrainageAreaArray[@]}
+do
+    echo $A
+    v.db.addcolumn map=plazuelas columns="distance_to_rivers_$A double precision"
+    v.distance from=plazuelas to=streams_$A upload=dist column=distance_to_rivers_$A
+done
+
+v.db.select map=plazuelas sep='\t' file=plazuelas.tsv
 
